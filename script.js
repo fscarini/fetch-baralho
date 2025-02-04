b0cc3e4d97a92723e984c4050a5b06195de83313
Fernando Carini <fernando2015lk@gmail.com>
2025-02-04 18:30:41 -0300

    resolucao do exercicio

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
-async function criarBaralho(){
+document.getElementById('tirar-carta').addEventListener('click', ()=>{
+    tirarUmaCartaDoBaralho()
+})
+
+let deckId = null;
+
+async function criarBaralho() {
     const url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
     const response = await fetch(url)
-    return await response.json()
+    const data = await response.json()
+    deckId = data.deck_id;
+    return data
 }
 
-async function tirarUmaCarta(deck_id){
+async function tirarUmaCarta(deck_id) {
     const url = `https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`
     const response = await fetch(url)
     return await response.json()
 }
 
-async function tirarUmaCartaDoBaralho(){
+async function criarBaralhoTirandoUmaCarta() {
     const baralho = await criarBaralho()
     const carta = await tirarUmaCarta(baralho.deck_id)
-    return await console.log(carta)
+    document.getElementById('carta').src = carta.cards[0].image
 }
 
-tirarUmaCartaDoBaralho()
+async function tirarUmaCartaDoBaralho() {
+    if(!deckId){
+        console.error("O baralho ainda não foi criado")
+        return;
+    }
+
+    const carta = await tirarUmaCarta(deckId)
+    if (carta.cards.length > 0) {
+        document.getElementById('carta').src = carta.cards[0].image;
+    } else {
+        console.log("Acabaram as cartas no baralho.");
+    }
+}
+
+criarBaralhoTirandoUmaCarta()
 
 
 
